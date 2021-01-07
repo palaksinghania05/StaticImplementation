1ab776fc6b7e6b02fa7df6cb218fdd5d5a709c21
palaksinghania05 <palak.singhania_cs.ccv19@gla.ac.in>
2021-01-07 13:07:31 +0530

    Initial commit

---
+++ src/StaticBlock.java
@@ -0,0 +1,14 @@
+public class StaticBlock {
+    //static block
+    
+    static {
+        System.out.println("Static Block 1");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main Method");
+    }
+    static {
+        System.out.println("Static Block 2");
+    }
+}
